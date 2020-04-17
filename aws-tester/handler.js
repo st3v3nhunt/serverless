6447cd71898e8f5dea2a0f1cc5ddb0bad69a78e0
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-17 08:48:44 +0100

    Message change

--- aws-tester/handler.js
+++ aws-tester/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello Cruel World!',
+        message: 'Hello Cruel World!!',
         input: event,
       },
       null,
